0174359a09a32054f22ac41b7ffa6367faf74a96
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-12-18 07:05:55 +0100

    enabled collectionid in media link sproc

--- Scripts/Database/AddAttachmentWithFileNameToAltCatNumber.sql
+++ Scripts/Database/AddAttachmentWithFileNameToAltCatNumber.sql
@@ -13,17 +13,19 @@ COMMENT ''
 BEGIN
 
 SET @altcatnr := CAST(AltCatNr AS CHAR CHARACTER SET UTF8);
+SET @collectionid := CAST(CollectionId AS CHAR CHARACTER SET UTF8);
 
 INSERT INTO attachment 
        (TimestampCreated, Version, AttachmentLocation, FileCreatedDate, GUID, 
 		  License, MimeType, origFilename, ScopeID, ScopeType, TableID, Title, CreatedByAgentID, IsPublic)
 VALUES (CURRENT_TIMESTAMP(), 0, FileName, DATE(CURRENT_TIMESTAMP()), (select md5(UUID())), 
-        'CC BY 4.0', 'image/jpeg', FileName, CollectionId, 0, 1, Filename, 10215, 1);
+        'CC BY 4.0', 'image/jpeg', FileName, @collectionid, 0, 1, Filename, 10215, 1);
 
 INSERT INTO collectionobjectattachment  
 		 (Timestampcreated, Version, CollectionMemberID, Ordinal, CreatedByAgentID, AttachmentID, CollectionobjectID) 
-VALUES (CURRENT_TIMESTAMP(), 0, (SELECT collectionid FROM collectionobject WHERE catalognumber = CatalogNr LIMIT 1), 
-        0, 10215, LAST_INSERT_ID(), (SELECT collectionobjectid FROM collectionobject WHERE catalognumber = CatalogNr LIMIT 1));
+VALUES (CURRENT_TIMESTAMP(), 0, @collectionid, 0, 10215, LAST_INSERT_ID(), (SELECT collectionobjectid FROM collectionobject WHERE AltCatalogNumber = @altcatnr LIMIT 1));
+
+-- END
 
 END
 //
