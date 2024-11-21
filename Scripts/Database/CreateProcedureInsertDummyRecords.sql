3d7bbec08acfe83c6d8b4a1e038096eb19331b89
FedorSteeman <fedor.steeman@gmail.com>
2024-11-21 10:40:05 +0100

    Refined sproc for inserting dummy records

--- Scripts/Database/CreateProcedureInsertDummyRecords.sql
+++ Scripts/Database/CreateProcedureInsertDummyRecords.sql
@@ -1,36 +1,50 @@
 DELIMITER //
 
-CREATE DEFINER=`root`@`%` PROCEDURE `InsertDummyRecords`(
-	IN `amount` INT,
-	IN `collectionID` INT,
-	IN `catalogerID` INT,
-	IN `agentID` INT,
-	IN `ProjectName` TINYTEXT,
-	IN `Remarks` TEXT
+CREATE OR REPLACE DEFINER=`root`@`%` PROCEDURE `InsertDummyRecords`(
+    IN `amount` INT,
+    IN `basenr` INT,
+    IN `collectionID` INT,
+    IN `catalogerID` INT,
+    IN `agentID` INT,
+    IN `ProjectName` TINYTEXT,
+    IN `Remarks` TEXT
 )
 LANGUAGE SQL
 NOT DETERMINISTIC
 CONTAINS SQL
 SQL SECURITY DEFINER
-COMMENT ''
+COMMENT 'copy from old server'
 BEGIN
-    
-	DECLARE highest INT DEFAULT 0;
-	
-   WHILE amount > 0 DO
+    DECLARE highest INT DEFAULT 0;
+    DECLARE catalogExists INT DEFAULT 0;
 
-    	SELECT CAST(TRIM(LEADING '0' FROM MAX(catalognumber)) AS UNSIGNED) INTO highest FROM collectionobject;
-    	 
-    	SET highest = highest + 1;
-    	
- 		INSERT INTO `collectionobject` (`TimestampCreated`, `TimestampModified`, `Version`, `CollectionMemberID`, `CatalogedDate`, 
- 				`CatalogNumber`, `ProjectNumber`, `Remarks`, `CollectionID`, `CreatedByAgentID`, `CatalogerID`, `YesNo1`, `YesNo3`) 
- 			VALUES (CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, collectionID, '2022-07-08',
- 				LPAD(highest, 9, 0),
-				ProjectName, Remarks, collectionID, agentID, catalogerID, 0, 1);
-       
-   	SET amount = amount - 1;
+    IF basenr <> 0 THEN
+        SET highest = basenr - 1;
+    END IF;
+
+    insert_loop: WHILE amount > 0 DO
+        IF basenr = 0 THEN
+	        SELECT COALESCE(MAX(CAST(TRIM(LEADING '0' FROM catalognumber) AS UNSIGNED)), 0) INTO highest FROM collectionobject;
+        END IF;
+
+        SET highest = highest + 1;
+
+        -- Check if the catalog number already exists
+        SELECT COUNT(*) INTO catalogExists FROM collectionobject WHERE CatalogNumber = LPAD(highest, 9, 0);
         
-   END WHILE;
+        IF catalogExists > 0 THEN
+            -- Skip the insertion if the catalog number exists
+            ITERATE insert_loop;
+        END IF;
 
+        INSERT INTO `collectionobject` (
+            `TimestampCreated`, `TimestampModified`, `Version`, `CollectionMemberID`, `CatalogedDate`, 
+            `CatalogNumber`, `ProjectNumber`, `Remarks`, `CollectionID`, `CreatedByAgentID`, `CatalogerID`, `YesNo1`, `YesNo3`
+        ) VALUES (
+            CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, collectionID, CURDATE(),
+            LPAD(highest, 9, 0), ProjectName, Remarks, collectionID, agentID, catalogerID, 0, 1
+        );
+
+        SET amount = amount - 1;
+    END WHILE;
 END//
